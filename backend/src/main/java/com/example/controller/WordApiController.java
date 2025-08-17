33b0c5b8415a6d521500b793a546fb805ac4170e
山下尊史 <yamashitatakahumi@MacBook-Air.local>
2025-08-17 09:38:12 +0900

    ログイン回りの修正

--- backend/src/main/java/com/example/controller/WordApiController.java
+++ backend/src/main/java/com/example/controller/WordApiController.java
@@ -2,20 +2,21 @@ package com.example.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.entity.Word;
 import com.example.service.WordService;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class WordApiController {
 
 	private final WordService wordService;
-
-	public WordApiController(WordService wordService) {
-		this.wordService = wordService;
-	}
 	
 	@GetMapping("/api/words")
 	public List<Word> getWords() {
