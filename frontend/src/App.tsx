6c523fcbae67dfa4cbf112b4a6ae977b0b7e03c7
山下尊史 <yamashitatakahumi@MacBook-Air.local>
2025-08-23 22:53:02 +0900

    conflict解消

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,11 +14,7 @@ const App: React.FC = () => {
   return (
     <>
       {showRegister ? (
-<<<<<<< HEAD
         <Register onRegister={() => setIsLoggedIn(true)} />
-=======
-        <Register onRegister={() => setIsLoggedIn(false)} />
->>>>>>> c0eef99c787acb39136987d17696290517a9c4a6
       ) : (
         <Login onLogin={() => setIsLoggedIn(true)} />
       )}
